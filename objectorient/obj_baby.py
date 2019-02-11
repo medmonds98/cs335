ac331306f52bea247dbdd16042d6ff9127c2fea5
Micah Edmonds <medmonds@una.edu>
2019-02-11 18:24:30 +0000

    modified obj_baby.py 2/11/2019

--- objectorient/obj_baby.py
+++ objectorient/obj_baby.py
@@ -3,31 +3,38 @@
 import urllib
 import re
 
-# Get web address from provided url
-def wget(url):
-    try:
-        text = urllib.urlopen(url)
-        #if text.info().gettype() == 'text/html':
-        #    print text.read()
-    except IOError:
-        print 'Could not access web address ', url
-    else:
-        return text
+class Babynames:
+   def __init__(self, year, url):
+       self.year = year
+       self.names_list = self._build_names(url)
+
+   def _build_names(self, url):
+       text = self._wget(url)
+       
+       pagetext = text.read()
+       beg = pagetext.find('<p><ol>')
+       end = pagetext.find('</ol>\n<div><br />')
+       
+       matches = re.findall(r'<strong>(\w*)</strong>', pagetext[beg:end])
+
+       return matches
+   
+   def output(self):
+       print str(self.year)
+       for item in self.names_list:
+           print item
+  
+   # Get web address from provided url
+   def _wget(self, url):
+       try:
+           text = urllib.urlopen(url)
+       except IOError:
+           print 'Could not access web address ', url
+       else:
+           return text
         
 
-text = wget('http://www.bounty.com/pregnancy-and-birth/baby-names/top-baby-names/100-most-popular-boys-names-so-far-in-2018')
+#text = wget('http://www.bounty.com/pregnancy-and-birth/baby-names/top-baby-names/100-most-popular-boys-names-so-far-in-2018')
 
-pagetext = text.read()
-beg = pagetext.find('<ol>')
-end = pagetext.find('</ol>')
-list = pagetext[beg:end]
-
-for item in list:
-    print item
-
-# class Babynames:
-#   def __init__(year):
-#       boy = {}
-#       girl = {}
-#
-#   year = None
+bbynames = Babynames(2018, 'http://www.bounty.com/pregnancy-and-birth/baby-names/top-baby-names/100-most-popular-boys-names-so-far-in-2018')
+bbynames.output()
