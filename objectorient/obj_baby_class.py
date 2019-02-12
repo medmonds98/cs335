ce1bd1e7755d1f46573c9c43dd191eec237c526b
Micah Edmonds <medmonds@una.edu>
2019-02-12 18:14:44 +0000

    Edited obj_baby_class.py 2/12/2019

--- objectorient/obj_baby_class.py
+++ objectorient/obj_baby_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python -tt
 
+# Initial web address for reference: https://www.babycentre.co.uk/popular-baby-names
+
 import urllib
 import re
 import sys
@@ -9,9 +11,7 @@ class Babynames :
         self.year = year
         
     
-
 def main():
-    
     # Manage console commands
     args = sys.argv[1:]
     
@@ -19,21 +19,42 @@ def main():
         print 'usage: [--summaryfile] URL'
         sys.exit(1)
     
+    summary = False
     if args[0] == '--summaryfile':
+        summary = True
         del args[0]
     
-    # Attempt to open the given url
-    try:
-        text = urllib.urlopen(args[0])
-        
-        # Break the url up into different links
-        if text.info().gettype() == 'text/html':
-            pages = re.findall(r'\d+\sto\s\d+</h2><ul>(.*?)</ul>', text.read())
-            for page in pages:
-                print page
+    if summary:
+        # Attempt to open the given url
+        try:
+            text = urllib.urlopen(args[0])
             
-    except IOError:
-        print "could not access web address"
+            if text.info().gettype() == 'text/html':
+                
+                # Grab each website link from the html of the given url
+                addresses = []
+                link_lists = re.findall(r'\d+\sto\s\d+</h2><ul>(.*?)</ul>', text.read())
+                
+                #Refine the search further
+                for item in link_lists:
+                    links = re.findall(r'<a href="(.*?)">', item)
+                    
+                    #add the grouped links to the adresses array
+                    for link in links:
+                        addresses.append(link)
+                        
+                print addresses
+                
+                # For each address, open the url and grab a list of babynames from that page
+                
+        except IOError:
+            print "could not access web address"
+            
+    else:
+        print 'unrecognized command \'', args[0], '\''
+        sys.exit(1)
+        
+        
         
 if __name__ == '__main__':
     main()
